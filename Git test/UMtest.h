fbc03b42eaee948b513a5cc33144c92a1468e31a
uros <retrej@gmail.com>
2014-07-31 13:21:23 +0200

    added a test class

---
+++ Git test/UMtest.h
@@ -0,0 +1,13 @@
+//
+//  UMtest.h
+//  Git test
+//
+//  Created by Uros marcic on 31/07/14.
+//  Copyright (c) 2014 UM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UMtest : NSObject
+
+@end
---
+++ Git test/UMtest.m
@@ -0,0 +1,13 @@
+//
+//  UMtest.m
+//  Git test
+//
+//  Created by Uros marcic on 31/07/14.
+//  Copyright (c) 2014 UM. All rights reserved.
+//
+
+#import "UMtest.h"
+
+@implementation UMtest
+
+@end
